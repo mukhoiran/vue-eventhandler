4e74c54a4f84a1aaa8df014b651085b00a5af2ff
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-08-10 06:13:28 +0700

    event on some keyboard button

--- apps.js
+++ apps.js
@@ -21,9 +21,23 @@ var app = new Vue({
   // }
 
   // <!-- =========== Event on Enter =========== -->
+  // methods:{
+  //   message: function(){
+  //     alert('You click enter button');
+  //   }
+  // }
+
+  //<!-- =========== event on some keyboard =========== -->
+  data:{
+    result:''
+  },
   methods:{
-    message: function(){
-      alert('You click enter button');
-    }
+    enter: function(){ this.result = 'Enter button' },
+    esc: function(){ this.result = 'Escape button' },
+    space: function(){ this.result = 'Space button'},
+    up: function(){ this.result = 'Up button' },
+    down: function(){ this.result = 'Down button' },
+    right: function(){ this.result = 'Right button' },
+    left: function(){ this.result = 'Left button' }
   }
 })
