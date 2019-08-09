b4c1a0a650b28e6476f5e11f119f1e53187a575a
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-08-10 05:20:21 +0700

    mouse onclick with methods

--- apps.js
+++ apps.js
@@ -2,8 +2,14 @@ var app = new Vue({
   el:'#app',
 
   // <!-- =========== Mouse Onclick =========== -->
-  data:{
-    message: '',
-  }
+  // data:{
+  //   message: '',
+  // }
 
+  // <!-- =========== Mouse Onclick with Methods =========== -->
+  methods: {
+    message: function() {
+       alert('Learning to use event click')
+    }
+  }
 })
